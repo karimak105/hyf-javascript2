2d4a7df8e042d863ed019966d617633b068cac11
Kari Mak <33581491+karimak105@users.noreply.github.com>
2018-01-13 23:18:10 +0100

    Add files via upload

---
+++ week1/week4.js
@@ -0,0 +1,2 @@
+var myBook =[];
+myBook =["harry_potter_chamber_secrets","harry_potter_philosophers_stone","harry_potter_prisoner_azkaban","harry_potter_goblet_fire","harry_potter_order_phoenix","harry_potter_halfblood_prince","harry_potter_deathly_hallows","harry_potter_cursed_child","harry_potter_prequel","harry_potter"];
